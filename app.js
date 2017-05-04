915b4d5100baa8f718e1a92d4554f843b551b9c6
Todd Gower <todd.h.gower@gmail.com>
2017-05-04 19:15:06 -0400

    add node app'

---
+++ app.js
@@ -0,0 +1,26 @@
+/**
+ * Created by toddgower on 5/4/17.
+ */
+'use strict';
+
+const express = require('express');
+const chalk = require('chalk');
+const figlet = require('figlet');
+
+const app = express();
+
+console.log(figlet.textSync("Starting...", {
+    font: "ogre"
+}));
+
+app.get('/', (req, res) => {
+    res.send("hello world!");
+});
+
+app.listen(process.env.PORT || 5000, () => {
+    console.log(chalk.green(`Server is running on port ${process.env.PORT || 5000}`));
+});
+
+
+
+module.exports = app;
---
+++ test/test.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const request = require('supertest');
+const app = require('../app.js');
+
+describe('GET /', () => {
+    it('respond with hello world', (done) => {
+        request(app).get('/').expect('hello world!', done);
+    });
+});
